e51829cd617f63bfcf8d2b59a5e052a28e18cb5a
garethjns <garethjns4@gmail.com>
2017-02-08 10:49:53 +0000

    Updated data subpath setting
    Subject folder names are set in functions genFileListSomeSingles and
    genFileListNoConcat as:
    
    sDir = [paths.dataDir, str, '_', subs{s}, '_New\'];
    Or:
    sDir = [paths.dataDir, str, '_', subs{s}, '\'];
    
    It matters if _New is included in the folder name, but not in the file
    names which are found with:
    
    files = dir([sDir, '*.mat']);
    
    And information is extracted around '_' and '_' or '.' (depnding on
    train or test).

--- featuresObject.m
+++ featuresObject.m
@@ -470,7 +470,10 @@ classdef featuresObject
                 fileList.Properties.VariableNames = vars(:,2);
                 fileList.Properties.VariableDescriptions = vars(:,3);
                 
-                sDir = [paths.dataDir, str, '_', subs{s}, '\'];
+                % Inflexible setting of data subpath
+                % sDir = [paths.dataDir, str, '_', subs{s}, '\'];
+                sDir = [paths.dataDir, str, '_', subs{s}, '_New\'];
+                % sDir = [paths.dataDir, str, '_', subs{s}, '\'];
                 
                 % files = dir([sDir, subs{s}, '*']);
                 files = dir([sDir, '*.mat']);
@@ -595,7 +598,9 @@ classdef featuresObject
                 fileList.Properties.VariableNames = vars(:,2);
                 fileList.Properties.VariableDescriptions = vars(:,3);
                 
-                sDir = [paths.dataDir, str, '_', subs{s}, '\'];
+                % Inflexible setting of data subpath
+                % sDir = [paths.dataDir, str, '_', subs{s}, '\'];
+                sDir = [paths.dataDir, str, '_', subs{s}, '_New\'];
                 
                 % files = dir([sDir, subs{s}, '*']);
                 files = dir([sDir, '*.mat']);
